eddc2dc8cacc5eac0e594b7592a18057b0fc53f9
luminxu <luminxu@link.cuhk.edu.hk>
2021-05-12 15:08:17 +0800

    paf detector

--- mmpose/core/evaluation/__init__.py
+++ mmpose/core/evaluation/__init__.py
@@ -1,5 +1,6 @@
-from .bottom_up_eval import (aggregate_results, get_group_preds,
-                             get_multi_stage_outputs)
+from .bottom_up_eval import (aggregate_results, aggregate_results_paf,
+                             get_group_preds, get_multi_stage_outputs,
+                             get_multi_stage_outputs_paf)
 from .eval_hooks import DistEvalHook, EvalHook
 from .mesh_eval import compute_similarity_transform
 from .pose3d_eval import keypoint_mpjpe
@@ -8,9 +9,20 @@ from .top_down_eval import (keypoint_auc, keypoint_epe, keypoint_pck_accuracy,
                             pose_pck_accuracy, post_dark_udp)
 
 __all__ = [
-    'EvalHook', 'DistEvalHook', 'pose_pck_accuracy', 'keypoints_from_heatmaps',
-    'keypoints_from_regression', 'keypoint_pck_accuracy', 'keypoint_auc',
-    'keypoint_epe', 'get_group_preds', 'get_multi_stage_outputs',
-    'aggregate_results', 'compute_similarity_transform', 'post_dark_udp',
-    'keypoint_mpjpe'
+    'EvalHook',
+    'DistEvalHook',
+    'pose_pck_accuracy',
+    'keypoints_from_heatmaps',
+    'keypoints_from_regression',
+    'keypoint_pck_accuracy',
+    'keypoint_auc',
+    'keypoint_epe',
+    'get_group_preds',
+    'get_multi_stage_outputs',
+    'get_multi_stage_outputs_paf',
+    'aggregate_results',
+    'aggregate_results_paf',
+    'compute_similarity_transform',
+    'post_dark_udp',
+    'keypoint_mpjpe',
 ]
